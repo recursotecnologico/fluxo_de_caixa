c7f949d234a17d5e676f0fb0c159a1d6101f4a1a
recursotecnologico <recursotecnologico.oficial@gmail.com>
2022-01-07 12:51:28 -0300

    Ajuste infraestrutura dos diretorios app

--- app/app_rotas.js
+++ app/app_rotas.js
@@ -1,6 +1,22 @@
 module.exports = app =>{
 
-    require('./home/homeRotas')(app);
+    const auth = require('./app_autenticacao');
     require('./login/loginRotas')(app);
+
+    app.get('/', auth.cookie, (req,res)=>{
+        res.render('views/home');
+    })
+
+    app.get('logout', auth.cookie_logout);
+
+    app.get('/planos_de_contas', async (req,res)=>{
+        return res.render('views/planos_de_contas');
+    });
+
+
+    
+    app.get('*', (req,res)=>{
+        return res.send('404');
+    })
     return app;
 }
--- app/logout/logoutRotas.js
+++
@@ -1,6 +0,0 @@
-module.exports = app =>{
-    const auth = require('../app_autenticacao');
-    app.get('logout', auth.cookie_logout);
-
-    return app;
-}
